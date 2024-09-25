f63273c58d47fccfcab397be2232ed196bb53f4c
WonHyeok Jang <jwhmark@gmail.com>
2024-09-25 13:05:42 +0900

    2024-09-25 개인과제6 5번

--- Prac5/OSSPrac_Sub5/src/name.java
+++ Prac5/OSSPrac_Sub5/src/name.java
@@ -10,6 +10,7 @@ public class name {
         String std_name;
         String std_num;
         String std_major;
+        String std_grade;
 
         System.out.print("이름을 입력하세요: ");
         std_name = br.readLine();
@@ -20,10 +21,14 @@ public class name {
         System.out.print("학과를 입력하세요: ");
         std_major = br.readLine();
 
+        System.out.print("학년을 입력하세요: ");
+        std_grade = br.readLine();
+
         sb.append("\n").append("<출력>");
         sb.append("\n").append("이름: ").append(std_name);
         sb.append("\n").append("학번: ").append(std_num);
         sb.append("\n").append("학과: ").append(std_major);
+        sb.append("\n").append("학년: ").append(std_grade);
 
         System.out.println(sb);
 
@@ -33,6 +38,7 @@ public class name {
         System.out.println("이름: " + std_name);
         System.out.println("학번: " + std_num);
         System.out.println("학과: " + std_major);
+        System.out.println("학년: " + std_grade);
         */
     }
 }
